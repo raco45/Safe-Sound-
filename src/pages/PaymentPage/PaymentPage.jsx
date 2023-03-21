4f7a861b9dab988b4a1bdc9a1763f247c582ccff
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2023-03-21 15:33:48 -0400

    Payment en proceso

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom"
+const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
 
 export function PaymentPage() {
   return (
