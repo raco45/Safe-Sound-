8bdd576444df68674e504247b67a542a10c4e409
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2023-03-24 18:03:18 -0400

    'Avance en pago'

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -3,23 +3,33 @@ import { PayPalButtons } from "@paypal/react-paypal-js";
 
 export function PaymentPage() {
   return (
-    <PayPalButtons
-      createOrder={(data, actions) => {
-        return actions.order.create({
-          purchase_units: [
-            {
-              amount: {
-                value: "13.99",
+    <div className="flex">
+      <h1>Información del pago</h1>
+      <div className=" flex w-1/3 h-screen bg-[#E1BCE8] justify-center flex-col text-center">
+        <h1 className="font-bold">Safe&Sound</h1>
+        <p>
+          Encuentra a los mejores especialistas que te brindaran terapias
+          psicologicas.
+        </p>
+      </div>
+      <PayPalButtons
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: "13.99",
+                },
               },
-            },
-          ],
-        });
-      }}
-      onApprove={async (data, actions) => {
-        const details = await actions.order.capture();
-        const name = details.payer.name.given_name;
-        alert("Transaction completed by " + name);
-      }}
-    />
+            ],
+          });
+        }}
+        onApprove={async (data, actions) => {
+          const details = await actions.order.capture();
+          const name = details.payer.name.given_name;
+          alert("Transaction completed by " + name);
+        }}
+      />
+    </div>
   );
 }
