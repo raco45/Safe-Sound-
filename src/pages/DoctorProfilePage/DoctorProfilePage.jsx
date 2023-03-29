c86e6fd4f5ba21ba1514e557e8ebed52b6a9192e
alejanndrasan <asantos@correo.unimet.edu.ve>
2023-03-29 11:05:23 -0400

    'doctorprofile2.1'

--- src/pages/DoctorProfilePage/DoctorProfilePage.jsx
+++ src/pages/DoctorProfilePage/DoctorProfilePage.jsx
@@ -9,20 +9,20 @@ export function DoctorProfilePage() {
   const { getSingleDoctor, isLoading, singleDoctor } = useUsuarios();
   const [cita, setCita] = useState("")
   const [planescogido, setPlanescogido] = useState("")
- 
-  const currentTime = new Date().toLocaleTimeString( 'en-US',{
+
+  const currentTime = new Date().toLocaleTimeString('en-US', {
     hour12: false,
   });
 
   const today = new Date()
   let date = new Date()
-  date.setDate(today.getDate()+1)
-  const year = date.toLocaleString('default',{year:'numeric'})
-  const month = date.toLocaleString('default',{month:'2-digit'})
-  const day = date.toLocaleString('default',{day:'2-digit'})
-  const tomorrow = year + '-'+month+'-'+day
+  date.setDate(today.getDate() + 1)
+  const year = date.toLocaleString('default', { year: 'numeric' })
+  const month = date.toLocaleString('default', { month: '2-digit' })
+  const day = date.toLocaleString('default', { day: '2-digit' })
+  const tomorrow = year + '-' + month + '-' + day
   console.log(tomorrow)
-  const minDate = tomorrow+'T08:00'
+  const minDate = tomorrow + 'T08:00'
   console.log(minDate)
 
   useEffect(() => {
@@ -30,9 +30,9 @@ export function DoctorProfilePage() {
       getSingleDoctor(doctorid);
     }
   }, []);
-  console.log("cita"+ cita);
+  console.log("cita" + cita);
 
-  
+
   return (
     <div>
       <div className="flex items-center  m-10">
@@ -57,46 +57,59 @@ export function DoctorProfilePage() {
             <h1 className="text-[#fae4fd] text-sm">
               Sobre mi: {singleDoctor && singleDoctor.description}
             </h1>
-            <h1 className="text-[#fae4fd] mt-2 uppercase tracking-widest text-lg font-bold">
-              Especialidades
-            </h1>
-            <hr className="w-2/6 mt-2 mb-2" />
-            <h1 className="text-[#fae4fd] text-center mb-1 text-sm">
-              {singleDoctor && singleDoctor.specialties}
-            </h1>
+            {singleDoctor && singleDoctor.validated === false &&(<img
+              src={singleDoctor && singleDoctor.diploma}
+              alt=""
+              className="mt-2 rounded-none mb-2 w-44 h-auto border-2 border-gray-300"
+            />)}
           </div>
         </div>
-        <div className="bg-[#f3d3f8] shadow-sm shadow-gray-500 rounded-md w-full m-2 p-10 h-auto">
-          <h1 className="text-[#a063a8] mt-2 uppercase tracking-widest text-lg font-bold">
-            Agenda una cita:
-          </h1>
-          <div className="flex flex-col">
-            <div className=" border-2 p-2 rounded-l border-[#b990c0]">
-              {" "}
-              {singleDoctor && singleDoctor.plansdescription}{" "}
-            </div>
-            <br />
-            <div>
-              <label>Escoge un plan </label>
-              <select className="bg-[#ede3ef] shadow-sm  text-[#a063a8] border-[#a063a8] border-2 rounded-lg" onChange={e=>setPlanescogido(e.target.value)}>
-                {singleDoctor &&
-                  singleDoctor.plans.map((plan) => {
-                    return <option value={plan}>Plan: {plan}</option>;
-                  })}
-              </select>
-            </div>
-            <br />
-            <div>
-              <input className="bg-[#ede3ef] shadow-sm " type="datetime-local" min={minDate} onChange={e=>setCita(e.target.value)} />
-              <button className="bg-[#ede3ef] shadow-sm  text-[#a063a8] border-[#a063a8] border-2 rounded-md pl-2 pr-2 m-2 hover:bg-[#a063a8] hover:text-[#ede3ef]">Agregar cita: {cita+' Plan: $'+planescogido} </button>
-              
-            </div>
-            <div>
-            <button className="bg-[#ede3ef] shadow-sm text-xl text-[#a063a8] border-[#a063a8] border-2 rounded-md pl-2 pr-2 ml-0 m-2 hover:bg-[#a063a8] hover:text-[#ede3ef]">Agendar</button>
+
+        {singleDoctor && singleDoctor.validated === true && (
+          <>
+            <div className="bg-[#f3d3f8] shadow-sm shadow-gray-500 rounded-md w-full m-2 p-10 h-auto">
+            <h1 className="text-[#a063a8] mt-2 uppercase tracking-widest text-lg font-bold">
+                  Especialidades
+                </h1>
+                <hr className="w-2/6 mt-2 mb-2" />
+                <h1 className="text-[#a063a8] mb-1 text-sm">
+                  {singleDoctor && singleDoctor.specialties}
+                </h1>
+              <h1 className="text-[#a063a8] mt-2 uppercase tracking-widest text-lg font-bold">
+                Agenda una cita:
+              </h1>
+              <div className="flex flex-col">
+                <div className=" border-2 p-2 rounded-l border-[#b990c0]">
+                  {" "}
+                  {singleDoctor && singleDoctor.plansdescription}{" "}
+                </div>
+                <br />
+                <div>
+                  <label>Escoge un plan </label>
+                  <select className="bg-[#ede3ef] shadow-sm  text-[#a063a8] border-[#a063a8] border-2 rounded-lg" onChange={e => setPlanescogido(e.target.value)}>
+                    <option/>
+                    {singleDoctor &&
+                      singleDoctor.plans.map((plan) => {
+                        return <option value={plan}>Plan: {plan}</option>;
+                      })}
+                  </select>
+                </div>
+                <br />
+                <div>
+                  <input className="bg-[#ede3ef] shadow-sm " type="datetime-local" min={minDate} onChange={e => setCita(e.target.value)} />
+                  <button className="bg-[#ede3ef] shadow-sm  text-[#a063a8] border-[#a063a8] border-2 rounded-md pl-2 pr-2 m-2 hover:bg-[#a063a8] hover:text-[#ede3ef]">Agregar cita: {cita + ' Plan: $' + planescogido} </button>
+
+                </div>
+                <div>
+                  <button className="bg-[#ede3ef] shadow-sm text-xl text-[#a063a8] border-[#a063a8] border-2 rounded-md pl-2 pr-2 ml-0 m-2 hover:bg-[#a063a8] hover:text-[#ede3ef]">Agendar</button>
+                </div>
+                </div>
+                </div>
+              </>)
+            }
+
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+  
   );
 }
